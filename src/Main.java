743bc56ccfad061ed908dc81405ab34907c70b48
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-29 18:14:54 +0900

    create : 2744

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            //만약에 소문자 범위 라면
+            if (chars[i] >= 'a' && chars[i] <= 'z') {
+                //대문자 A 에 (입력값 - 소문자 a) 만큼의 거리를 더한다.
+                //(입력값 - 소문자 a) 가 거리
+                chars[i] = (char) ('A' + chars[i] - 'a');
+            } else {
+                //소문자 a 에 (입력값 - 대문자 A) 만큼의 거리를 더한다.
+                chars[i] = (char) ('a' + chars[i] - 'A');
+            }
+        }
+        System.out.println(chars);
     }
 }
