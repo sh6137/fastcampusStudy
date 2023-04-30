528120319b5d8bb2978a596b9e74f1f02df10f0a
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-30 22:52:56 +0900

    feat : 13223

---
+++ src/day2/Main2.java
@@ -0,0 +1,32 @@
+package day2;
+
+import java.util.Scanner;
+
+public class Main2 {
+    //13223
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String[] current = in.next().split(":");
+        String[] target = in.next().split(":");
+
+        int currentHour = Integer.parseInt(current[0]);
+        int currentMinute = Integer.parseInt(current[1]);
+        int currentSecond = Integer.parseInt(current[2]);
+
+        int targetHour = Integer.parseInt(target[0]);
+        int targetMinute = Integer.parseInt(target[1]);
+        int targetSecond = Integer.parseInt(target[2]);
+
+        int currentSecondAmount = currentHour * 3600 + currentMinute * 60 + currentSecond;
+        int targetSecondAmount = targetHour * 3600 + targetMinute * 60 + targetSecond;
+
+        int NeedSecondAmount = targetSecondAmount - currentSecondAmount;
+        if (NeedSecondAmount <= 0) NeedSecondAmount += 24 * 3600;
+
+        int needHour = NeedSecondAmount / 3600;
+        int needMinute = (NeedSecondAmount % 3600) / 60;
+        int needSecond = NeedSecondAmount % 60;
+
+        System.out.println(String.format("%02d:%02d:%02d", needHour, needMinute, needSecond));
+    }
+}
