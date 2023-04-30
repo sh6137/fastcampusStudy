e4867f29b4e17f6a560fd0eb906c2e4843952bf9
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-04-30 19:41:52 +0900

    feat : 1919 부연설명

--- src/day1/Main2.java
+++ src/day1/Main2.java
@@ -6,22 +6,28 @@ public class Main2 {
     //    1919
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String str1 = in.nextLine();
-        String str2 = in.nextLine();
+        String str1 = in.nextLine(); //abc
+        String str2 = in.nextLine(); //b
         int result = 0;
 
-        int[] charA = new int[26];
-        int[] charB = new int[26];
+        int[] intA = new int[26];
+        int[] intB = new int[26];
 
+        // intA 배열 [] 번째에 값음 증가시킨다.
         for (int i = 0; i < str1.length(); i++) {
-            charA[str1.charAt(i) - 'a']++;
+            intA[str1.charAt(i) - 'a']++;
         }
+        // intB 배열 [] 번째에 값음 증가시킨다.
         for (int i = 0; i < str2.length(); i++) {
-            charB[str2.charAt(i) - 'a']++;
+            intB[str2.charAt(i) - 'a']++;
         }
+        // intA = [1][1][1]
+        // intB = [0][1][0]
 
-        for (int i = 0; i < charA.length; i++) {
-            result += Math.abs(charA[i] - charB[i]);
+        // intA[] 값과 intB[] 값을 뺀다.
+        // 그리고 result 에 값을 더한다.
+        for (int i = 0; i < intA.length; i++) {
+            result += Math.abs(intA[i] - intB[i]);
         }
         System.out.println(result);
     }
