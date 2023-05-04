f9cac45d9150f80871e1e5021f3b93fc89114c6b
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-05-04 15:24:49 +0900

    feat : 10158

--- src/day3/Main.java
+++ src/day3/Main.java
@@ -4,31 +4,24 @@ import java.util.Scanner;
 
 public class Main {
     //    10158
+    //todo : 10158 개미 복습하기
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String space = in.nextLine();
-        String target = in.nextLine();
-        int time = Integer.parseInt(in.nextLine());
-        String[] spaceArray = space.split(" ");
-        int[] spaceNum = new int[]{Integer.parseInt(spaceArray[0]), Integer.parseInt(spaceArray[1])};
-        String[] targetArray = target.split(" ");
-        int[] targetNum = new int[]{Integer.parseInt(targetArray[0]), Integer.parseInt(targetArray[1])};
-        int moveX = 1;
-        int moveY = 1;
-        for (int i = 0; i < time; i++) {
-            if (spaceNum[0] == targetNum[0]) {
-                moveX = -1;
-            }
-            if (spaceNum[1] == targetNum[1]) {
-                moveY = -1;
-            }
-            if (targetNum[0] == 0) {
+        int W = in.nextInt(); //격자의 가로길이
+        int H = in.nextInt(); //격자의 세로길이
+        int P = in.nextInt(); //개미좌표 x
+        int Q = in.nextInt(); //개미좌표 y
+        int T = in.nextInt(); //시간
 
-            }
+        int p = (P + T) % (2 * W);
+        int q = (Q + T) % (2 * H);
 
-            targetNum[0] = targetNum[0] + moveX;
-            targetNum[1] = targetNum[1] + moveY;
+        if (p > W) {
+            p = 2 * W - p;
         }
-        System.out.println(targetNum[0] + " " + targetNum[1]);
+        if (q > H) {
+            q = 2 * H - q;
+        }
+        System.out.println(p + " " + q);
     }
 }
